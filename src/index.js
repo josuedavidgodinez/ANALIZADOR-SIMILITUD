ccc371465de17393859852a814e0518342ea7981
David Godinez <godinezjosue@hotmail.com>
2020-10-03 02:38:46 -0600

    verificar parentesco de 2 imagenes

--- src/index.js
+++ src/index.js
@@ -1,22 +1,69 @@
 var imagenes = Array();
+var imagen1;
+var imagen2;
 
 async function loadImage(event, destino) {
+
   var myURL = window.URL || window.webkitURL
   console.log('winurl=' + myURL);
   var image = document.getElementById(destino);
   let source = myURL.createObjectURL(event.target.files[0]);
   image.src = source;
-  var imagen;
-  imagen = await analyze(event.target.files[0]);
-  imagenes.push(imagen);
+
+  destino == 'imagen1' ? (
+    imagen1 = null,
+    imagen1 = await analyze(event.target.files[0])
+  )
+    : (
+      imagen2 = null,
+      imagen2 = await analyze(event.target.files[0])
+    );
+
+
 
 
 };
 
 
-function predict(event) {
+async function predict(event) {
   event.preventDefault();
-  console.log(imagenes);
+  var face1 = imagen1[0].faceId;
+  var face2 = imagen2[0].faceId;
+
+  var son_identicos;
+  var porcentaje_parecido;
+  var return_from_verify = await call_api_for_verify(face1, face2);
+
+  son_identicos = return_from_verify.isIdentical;
+  porcentaje_parecido = return_from_verify.confidence;
+
+  son_identicos ? alert("¡Son idéntidos! por un porcentaje de " + porcentaje_parecido) : alert(" No se parecen mucho, por un porcentaje de " + porcentaje_parecido)
+
+
+
+}
+
+async function call_api_for_verify(faceid1, faceid2) {
+
+
+  var resultado = await $.ajax({
+    url: "https://2dociclourl2020.cognitiveservices.azure.com/face/v1.0/verify",
+
+    method: "POST",
+    type: "POST",
+    beforeSend: function (xhrObj) {
+      xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "1113e698f4574f19af28cb44de439789");
+    },
+    contentType: "application/json",
+    data: '{"faceId1": ' + '"' + faceid1 + '","faceId2": ' + '"' + faceid2 + '"}',
+
+  }).done(function (data) {
+    // Show formatted JSON on webpage.
+    data;
+  });
+
+  console.log(resultado);
+  return resultado;
 
 }
 
